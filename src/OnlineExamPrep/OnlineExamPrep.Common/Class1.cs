46448e21a5a025d518d604b6728c70cab23e2944
Ivana Simic <isimic413@gmail.com>
2015-11-15 15:37:36 +0100

    initial folder structure

---
+++ src/OnlineExamPrep/OnlineExamPrep.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExamPrep.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/OnlineExamPrep/OnlineExamPrep.Models.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExamPrep.Models.Common
+{
+    public class Class1
+    {
+    }
+}
