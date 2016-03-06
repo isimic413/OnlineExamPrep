4b496caf2c7da3c80c37f2f307936874a45abd4f
Ivana Simic <isimic413@gmail.com>
2016-03-06 01:53:14 +0100

    fixed bug related to lookups, simulator styling changes

--- src/OnlineExamPrep/app/common/data/Lookups.js
+++ src/OnlineExamPrep/app/common/data/Lookups.js
@@ -3,23 +3,42 @@
 
         var self = this;
 
-        var testingAreas = {};
-        var questionTypes = {};
+        var testingAreas = null;
+        var questionTypes = null;
 
         this.setLookups = function (data) {
+            testingAreas = {};
+            questionTypes = {};
+
             _.each(data.testingArea, function (ta) {
                 testingAreas[ta.abbreviation] = angular.copy(ta);
             });
             _.each(data.questionType, function (qt) {
                 questionTypes[qt.abbreviation] = angular.copy(qt);
             });
+            $window.localStorage.lookups = angular.toJson({
+                questionTypes: questionTypes,
+                testingAreas: testingAreas
+            });
         };
 
         this.getTestingAreas = function () {
+            if (!testingAreas) {
+                loadLookups();
+            }
             return testingAreas;
         };
 
         this.getQuestionTypes = function () {
+            if (!questionTypes) {
+                loadLookups();
+            }
             return questionTypes;
         };
+
+        function loadLookups() {
+            var lookups = angular.fromJson($window.localStorage.lookups);
+            testingAreas = lookups.testingAreas;
+            questionTypes = lookups.questionTypes;
+        }
     });
